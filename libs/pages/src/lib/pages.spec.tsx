5ca3e28fe2c47faa61dfb35d69a077a2d2ecd8e9
Ayellet Tal <tal.ayellet@gmail.com>
2025-12-01 20:49:27 +0200

    chore: Inits pages library

---
+++ libs/pages/src/lib/pages.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import Pages from './pages';
+
+describe('Pages', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Pages />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/pages/src/lib/pages.tsx
@@ -0,0 +1,11 @@
+import styles from './pages.module.css';
+
+export function Pages() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Pages!</h1>
+    </div>
+  );
+}
+
+export default Pages;

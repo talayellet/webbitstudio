6db5f5b4e8a0db9a57af4021e554c489402c8ba9
Ayellet Tal <tal.ayellet@gmail.com>
2025-12-07 12:51:39 +0200

    fix: Home click after footer nav-out

--- libs/pages/src/lib/components/home/components/header/components/logo.tsx
+++ libs/pages/src/lib/components/home/components/header/components/logo.tsx
@@ -1,3 +1,4 @@
+import { useNavigate, useLocation } from 'react-router-dom';
 import * as styles from '../../../utils/styles';
 import { useScrollToTop } from '@webbitstudio/shared-utils';
 
@@ -9,11 +10,23 @@ interface LogoProps {
 
 export const Logo = ({ title, subtitle, ariaLabel }: LogoProps) => {
   const { scrollToTop } = useScrollToTop();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = () => {
+    // If we're not on the home page, navigate to it
+    if (location.pathname !== '/') {
+      navigate('/');
+    } else {
+      // If we're already on the home page, just scroll to top
+      scrollToTop();
+    }
+  };
 
   return (
     <button
       type="button"
-      onClick={scrollToTop}
+      onClick={handleClick}
       className={styles.header.logo.container}
       aria-label={ariaLabel}
     >
